c9cab6f14b1df652d370c21cb9d545252e7364fb
guedera <guilhermegg5@al.insper.edu.br>
2025-02-17 17:21:11 -0300

    quasela

--- HandOut/Transmit.py
+++ HandOut/Transmit.py
@@ -2,7 +2,7 @@ from enlace import *
 import time
 import numpy as np
 
-serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+serialName = "/dev/ttyACM0"
 
 def main():
     try:
@@ -10,29 +10,18 @@ def main():
         com1 = enlace(serialName)
         
         com1.enable()
-        print("Abriu a comunicação")
-
-        time.sleep(.2)
-        com1.sendData(b'00')
-        time.sleep(1)
-
-        print('\n')
-        print("Enviou o bit de sacrifício.")
         
-        numeros_f = []
-
-        txBuffer = b'\x13' #
+        print("Abriu a comunicação")
+        txBuffer = b'\x13' 
         
         print("Meu array de bytes tem tamanho {}" .format(len(txBuffer)))
         
         print("Transmissão iniciada!")
-        com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
+        com1.sendData(np.asarray(txBuffer))
         
         txSize = com1.tx.getStatus()
         print('enviou = {} bytes' .format(txSize))
         
-        txLen = len(txBuffer)
-
         print("-------------------------")
         print("Comunicação encerrada")
         print("-------------------------")
