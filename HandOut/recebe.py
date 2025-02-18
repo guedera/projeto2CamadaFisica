c97ff7c494c525496e8ff6ef04e85c4a8eb55f8e
guedera <guilhermegg5@al.insper.edu.br>
2025-02-17 21:30:19 -0300

    rebase struct

---
+++ HandOut/recebe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from enlace import enlace
+import time
+
+def main():
+    # Substitua 'COMx' pela porta serial correta do seu Arduino2
+    serialName = "/dev/ttyACM0"
+    print("Receptor inicializado na porta {}".format(serialName))
+    
+    # Instancia a camada de enlace e habilita a comunicação
+    com = enlace(serialName)
+    com.enable()
+
+    print("Aguardando recebimento de dados...")
+    # Aguarda e lê 1 byte recebido
+    rxBuffer, nRx = com.getData(1)
+    
+    print("Recebido {} byte(s): {}".format(nRx, rxBuffer))
+
+    # Desabilita a comunicação
+    com.disable()
+
+if __name__ == '__main__':
+    main()
---
+++ HandOut/transmite.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from enlace import enlace
+import time
+
+def main():
+    # Substitua 'COMx' pela porta serial correta do seu Arduino1
+    serialName = "/dev/ttyACM0"
+    print("Transmissor inicializado na porta {}".format(serialName))
+    
+    # Instancia a camada de enlace e habilita a comunicação
+    com = enlace(serialName)
+    com.enable()
+
+    # Cria buffer com 1 byte: "\x13"
+    txBuffer = b"\x13"
+    print("Transmitindo o byte: {}".format(txBuffer))
+    
+    # Envia o dado
+    com.sendData(txBuffer)
+
+    # Aguarda até que a transmissão seja concluída
+    while com.tx.getIsBussy():
+        time.sleep(0.1)
+    print("Transmissão finalizada!")
+
+    # Desabilita a comunicação
+    com.disable()
+
+if __name__ == '__main__':
+    main()
