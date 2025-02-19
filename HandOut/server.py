4e64b84fed412f5cd38909162553fbe2c1af0eef
enzochristo <enzosc1@al.insper.edu.br>
2025-02-19 19:13:43 -0300

    server

--- HandOut/server.py
+++ HandOut/server.py
@@ -1,39 +1,14 @@
-#####################################################
-# Camada Física da Computação
-#Carareto
-#11/08/2022
-#Aplicação
-#####################################################
-
-
-#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
-#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
-
-
 from enlace import *
 import time
-import numpy as np
+from IEEEToFloat import bytes_to_float
 
-# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
-
-#use uma das 3 opcoes para atribuir à variável a porta usada
-# serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
-#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "/dev/ttyACM0"                  # Windows(variacao de)  detectar sua porta e substituir aqui
-
+serialName = "COM7"
 
 def main():
     try:
         print("Iniciou o main")
-        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
-        #para declarar esse objeto é o nome da porta.
         com1 = enlace(serialName)
         
-    
-        # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
 
         print("esperando 1 byte de sacrifício")
@@ -47,31 +22,27 @@ def main():
         print("Recepção iniciada!")
         print("\n")
 
-        rxBuffer, nRx = com1.getData(1)
-        print("recebeu {} bytes" .format(len(rxBuffer)))
+        rxBuffer, nRx = com1.getData(12)
+        print("recebeu {} bytes".format(nRx))
         print("\n")
 
-            
-        # obtido = com1.rx.getNData(rxBuffer)
+        if nRx == 12:
+            floats = [bytes_to_float(rxBuffer[i:i+4]) for i in range(0, 12, 4)]
+            for float in floats:
+                print("Valores floats recebidos:", {float} )
+        else:
+            print("Número de bytes recebido é inadequado.")
 
         print("\n")
         print("\n")
-
-        num = int.from_bytes(rxBuffer, byteorder='big')
-
-        print(f"OS DADOS OBTIDOS FORAM: {num}")
-        print("-------------------------")
         print("Comunicação encerrada")
         print("-------------------------")
-        print(type(rxBuffer))
         com1.disable()
         
     except Exception as erro:
         print("ops! :-\\")
         print(erro)
         com1.disable()
-        
 
-    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
 if __name__ == "__main__":
     main()
