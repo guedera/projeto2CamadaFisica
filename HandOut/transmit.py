6ed5928f4759d167a6c87f2b8b5659aa88bf588f
guedera <guilhermegg5@al.insper.edu.br>
2025-02-19 20:44:07 -0300

    quickpush

--- HandOut/transmit.py
+++ HandOut/transmit.py
@@ -2,40 +2,66 @@ from enlace import *
 import time
 import numpy as np
 from floatToIEEE import float_to_ieee754
+from IEEEToFloat import ieee754_to_float
+import os
 
 serialName = "/dev/ttyACM0"
 
 lista = [1.3424, 54.45544, 200.002]
+soma_lista = sum(lista)
 
 def main():
     try:
         print("Iniciou o main")
         com1 = enlace(serialName)
-        
         com1.enable()
+        os.system('clear')
 
-        print("comecando BS")
-        time.sleep(2)
+        print("Enviando o byte de sacrifício")
         com1.sendData(b'00')
+        print("Byte de sacrifício enviado!\n")
         time.sleep(2)
-        print("enviando bit de sacrificio")
-        
-        print("Abriu a comunicação, vou enviar 12 bytes.")
+        os.system('clear')
+
+        print("Vou mandar quantos números serão enviados!")
+        txBuffer = bytearray([len(lista)])
+        print("Estou enviando {} números flutuantes!\n" .format(len(lista)))
+        com1.sendData(np.asarray(txBuffer))
+        print("Enviado!")
         time.sleep(2)
+        os.system('clear')
+
+        print("Abriu a comunicação! Vou enviar a lista de Floats!\n")
         # Converte a lista de floats para o formato IEEE 754
         txBuffer = bytearray(float_to_ieee754(lista))
-        print(txBuffer)
-        print(len(txBuffer))
         
-        print("Meu array de bytes tem tamanho {}" .format(len(txBuffer)))
+        print("Meu array de bytes tem tamanho {}\n" .format(len(txBuffer)))
         
-        print("Transmissão iniciada!")
         com1.sendData(np.asarray(txBuffer))
+        print("Enviado!")
         time.sleep(2)
-        
+        os.system('clear')
+
         txSize = com1.tx.getStatus()
-        print('enviou = {} bytes' .format(txSize))
-        
+        print('enviou = {} bytes!' .format(txSize))
+        time.sleep(2)
+        os.system('clear')
+
+        print("Esperando a soma!")
+        rxBuffer, nRx = com1.getData(1)
+        os.system('clear')
+
+        print("Recebeu a soma!")
+        retorno = ieee754_to_float(rxBuffer)
+        print("O resultado da soma é: ", retorno)
+        time.sleep(2)
+
+        if soma_lista == retorno:
+            print("Soma correta!")
+        else:
+            print("Soma errada!")
+        time.sleep(2)
+
         print("-------------------------")
         print("Comunicação encerrada")
         print("-------------------------")
