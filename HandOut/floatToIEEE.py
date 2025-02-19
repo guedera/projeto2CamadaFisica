c0117d38561f363a74998355595f3748d66b6599
guedera <guilhermegg5@al.insper.edu.br>
2025-02-19 18:55:13 -0300

    quickpush

--- HandOut/floatToIEEE.py
+++ HandOut/floatToIEEE.py
@@ -1,29 +1,19 @@
 import struct
 
-def float_to_ieee754(number):
-    # Usa a biblioteca struct para empacotar o número em formato IEEE 754
-    packed = struct.pack('>f', number)
-    # Converte os bytes empacotados para uma lista de inteiros (bytes)
-    byte_list = list(packed)
-    return byte_list
+def float_to_ieee754(numbers):
+    ieee_list = []
+    for number in numbers:
+        # Usa a biblioteca struct para empacotar o número em formato IEEE 754
+        packed = struct.pack('>f', number)
+        # Converte os bytes empacotados para uma lista de inteiros (bytes)
+        byte_list = list(packed)
+        # Converte cada byte para hexadecimal e adiciona à lista
+        ieee_list.extend([hex(byte) for byte in byte_list])
+    return ieee_list
 
-
-#---------------------------------------------#
-
-#Teste
-#lista de floats
+# Exemplo de uso
 x = [1.12312, 1.23, 23.5443]
+ieee754_list = float_to_ieee754(x)
 
-#lista que será enviada
-pra_enviar = []
-
-#converte em ieee754 e adiciona na lista pra_enviar
-for i in x:
-    pra_enviar += (float_to_ieee754(i))
-
-#imprime a lista pra_enviar
-print(pra_enviar)
-
-for j in pra_enviar:
-    print(hex(j))
-#---------------------------------------------#
+# Imprime a lista de bytes em hexadecimal
+print(ieee754_list)
--- HandOut/server.py
+++ HandOut/server.py
@@ -14,15 +14,10 @@ from enlace import *
 import time
 import numpy as np
 
-# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
+#/dev/ttyACM0 ou COM7
+serialName = "/dev/ttyACM0"
 
-#use uma das 3 opcoes para atribuir à variável a porta usada
-# serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
-#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "/dev/ttyACM0"                  # Windows(variacao de)  detectar sua porta e substituir aqui
+pra_enviar = [1.453522,45.000012,222.020301]
 
 
 def main():
