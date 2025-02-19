46e8fc65648ba2f4955f803c82b51f784080b3b2
guedera <guilhermegg5@al.insper.edu.br>
2025-02-19 19:08:49 -0300

    quickpush

--- HandOut/floatToIEEE.py
+++ HandOut/floatToIEEE.py
@@ -7,13 +7,14 @@ def float_to_ieee754(numbers):
         packed = struct.pack('>f', number)
         # Converte os bytes empacotados para uma lista de inteiros (bytes)
         byte_list = list(packed)
-        # Converte cada byte para hexadecimal e adiciona à lista
-        ieee_list.extend([hex(byte) for byte in byte_list])
+        # Adiciona cada byte à lista
+        ieee_list.extend(byte_list)
     return ieee_list
 
 # Exemplo de uso
-x = [1.12312, 1.23, 23.5443]
-ieee754_list = float_to_ieee754(x)
+# x = [1.12312, 1.23, 23.5443]
+# ieee754_list = float_to_ieee754(x)
 
-# Imprime a lista de bytes em hexadecimal
-print(ieee754_list)
+# # Imprime a lista de bytes
+# print(ieee754_list)
+# print(type(ieee754_list[0]))
